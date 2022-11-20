8f19d5069aa2385e8759a6f2b3efd640bbfdf6e3
JBlades10 <juan.velasquez@snhu.edu>
2022-11-20 09:24:34 -0500

    Fixed Typos, Loaded partials

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ const logger = require("morgan");
 const indexRouter = require("./app_server/routes/index");
 const usersRouter = require("./app_server/routes/users");
 const travelRouter = require("./app_server/routes/travel");
-const { hbs } = require("diagnostics_channel");
+
+var hbs = require('hbs');
 
 const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "app_server", "views"));
 
+app.set("view engine", "hbs");
+
 // register handlebars partials (https://www.npmjs.com/package/hbs)
 hbs.registerPartials(path.join(__dirname, 'app_server','views/partials'));
 
-app.set("view engine", "hbs");
+
 
 app.use(logger("dev"));
 app.use(express.json());
