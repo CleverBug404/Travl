6d5d60d2f8fadffb2858ef3e7f5359782c01e8a1
JBlades10 <juan.velasquez@snhu.edu>
2022-11-22 18:36:04 -0500

    connected to mongodb

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require("morgan");
 const indexRouter = require("./app_server/routes/index");
 const usersRouter = require("./app_server/routes/users");
 const travelRouter = require("./app_server/routes/travel");
+require('./app_server/models/db');
 
 var hbs = require('hbs');
 
