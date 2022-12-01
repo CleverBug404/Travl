0fe57231fe3cc2495c24683f20f1ac09beab83ca
JBlades10 <juan.velasquez@snhu.edu>
2022-12-01 10:54:31 -0500

    Separated backend logic:routers,controllers,models

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+var hbs = require('hbs');
+require('./app_api/models/db');
 
-const indexRouter = require("./app_server/database/models/index");
+const indexRouter = require("./app_server/routes/");
 const usersRouter = require("./app_server/routes/users");
 const travelRouter = require("./app_server/routes/travel");
-require('./app_server/database/db');
-
-var hbs = require('hbs');
+const apiRouter = require('./app_api/routes/index')
 
 const app = express();
 
@@ -32,6 +32,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/travel", travelRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
